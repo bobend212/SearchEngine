071274e89f0826f657207411b2a1a09b62c59049
Mateusz Konopka <makonopkadev@gmail.com>
2022-08-25 12:02:07 +0200

    Searching works without pagination.

--- SearchEngine/Pages/Index.cshtml.cs
+++ SearchEngine/Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EuropePMC;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SearchEngine.Pages
@@ -7,14 +7,42 @@ namespace SearchEngine.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        private EuropePMC.WSCitationImpl service = new WSCitationImplClient();
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public IList<result> Collection { get; set; }
+        public int TotalCount { get; set; }
 
+        public async Task OnGetAsync(string searchString)
+        {
+            await LoadPublications("SRC:*");
+        }
+        public async Task OnPostSearchPublication(string test2)
+        {
+            if (!String.IsNullOrWhiteSpace(test2))
+            {
+                await LoadPublications(test2);
+            }
+            else
+            {
+                await LoadPublications("SRC:*");
+            }
+        }
+
+        private async Task LoadPublications(string searchText)
+        {
+            searchPublicationsResponse1 test = await service.searchPublicationsAsync(new searchPublicationsRequest()
+            {
+                queryString = searchText,
+                cursorMark = "*"
+            });
+
+            Collection = test.@return.resultList;
+            TotalCount = test.@return.hitCount;
         }
     }
 }
