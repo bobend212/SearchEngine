871bf0bcb6fe7519d3be03a3411be76f6e8e010f
Mateusz Konopka <makonopkadev@gmail.com>
2022-08-26 11:22:33 +0200

    Work in progress

--- SearchEngine/Pages/Index.cshtml.cs
+++ SearchEngine/Pages/Index.cshtml.cs
@@ -5,27 +5,77 @@ namespace SearchEngine.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
         private EuropePMC.WSCitationImpl service = new WSCitationImplClient();
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
 
         public IList<result> Collection { get; set; }
         public int TotalCount { get; set; }
+        //public string NextPageString { get; set; }
+        //public string SearchText { get; set; }
 
-        public async Task OnGetAsync(string searchString)
+        private searchPublicationsRequest request;
+
+        static string qString = "SRC:*";
+        static string qMark = "*";
+
+        public IndexModel()
         {
-            await LoadPublications("SRC:*");
+            request = new searchPublicationsRequest();
+            request.queryString = qString;
+            request.cursorMark = qMark;
         }
-        public async Task OnPostSearchPublication(string test2)
+
+        private async Task LoadPublications(string searchText)
         {
-            if (!String.IsNullOrWhiteSpace(test2))
+            searchPublicationsResponse1 response = await service.searchPublicationsAsync(request);
+
+            //NextPageString = response.@return.nextCursorMark;
+            //SearchText = searchText;
+
+            qString = searchText;
+
+
+            //searchPublicationsResponse1 response2 = await service.searchPublicationsAsync(new searchPublicationsRequest()
+            //{
+            //    queryString = qString,
+            //    cursorMark = qMark
+            //});
+
+            qMark = response.@return.nextCursorMark;
+
+            Collection = response.@return.resultList;
+            TotalCount = response.@return.hitCount;
+        }
+
+        public async Task OnGetAsync()
+        {
+            //await LoadPublications("SRC:*");
+
+            request.queryString = "SRC:*";
+            request.cursorMark = "*";
+
+            searchPublicationsResponse1 response = await service.searchPublicationsAsync(request);
+
+            request.queryString = "SRC:*";
+            request.cursorMark = qMark;
+
+            //NextPageString = response.@return.nextCursorMark;
+            //SearchText = searchText;
+
+            //searchPublicationsResponse1 response2 = await service.searchPublicationsAsync(new searchPublicationsRequest()
+            //{
+            //    queryString = "SRC:*",
+            //    cursorMark = "*"
+            //});
+
+            Collection = response.@return.resultList;
+            TotalCount = response.@return.hitCount;
+        }
+
+        public async Task OnPostSearchPublication(string search)
+        {
+            if (!String.IsNullOrWhiteSpace(search))
             {
-                await LoadPublications(test2);
+                await LoadPublications(search);
             }
             else
             {
@@ -33,16 +83,24 @@ namespace SearchEngine.Pages
             }
         }
 
-        private async Task LoadPublications(string searchText)
+        public async Task OnPostNextPage()
         {
-            searchPublicationsResponse1 test = await service.searchPublicationsAsync(new searchPublicationsRequest()
-            {
-                queryString = searchText,
-                cursorMark = "*"
-            });
+            searchPublicationsResponse1 response = await service.searchPublicationsAsync(request);
 
-            Collection = test.@return.resultList;
-            TotalCount = test.@return.hitCount;
+            //NextPageString = response.@return.nextCursorMark;
+            //SearchText = searchText;
+
+            //qString = qString;
+            qMark = response.@return.nextCursorMark;
+
+            //searchPublicationsResponse1 response2 = await service.searchPublicationsAsync(new searchPublicationsRequest()
+            //{
+            //    queryString = qString,
+            //    cursorMark = qMark
+            //});
+
+            Collection = response.@return.resultList;
+            TotalCount = response.@return.hitCount;
         }
     }
 }
